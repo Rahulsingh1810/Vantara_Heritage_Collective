1028735ab2fd315b9cac98990648007232b63fe8
Rahul Kumar Singh <rahulkumar.singh@cstep.in>
2026-02-02 15:06:10 +0530

    done

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,6 @@ export default function Footer() {
     <footer className="mt-20 bg-[var(--color-wine-red)] text-[var(--color-ivory)]">
       <div className="mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8">
         <div className="mb-8 grid grid-cols-1 gap-8 md:grid-cols-4">
-          
           {/* Brand */}
           <div>
             <Link href="/" className="inline-block">
